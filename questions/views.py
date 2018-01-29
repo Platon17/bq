fbc9983ae015d62582c2a9aa92e556b283e0be1b
KlimovAS <35884329+KlimovAS@users.noreply.github.com>
2018-01-29 17:44:28 +1000

    questions are ready

--- questions/views.py
+++ questions/views.py
@@ -13,17 +13,21 @@ def index(request):
 
 
 def detail(request, question_id):
+	#return answer 
     return render(request, "answer.html", {"question": get_object_or_404(Question, pk=question_id)})
 
 
 def answer(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
+		# answer 
         answer = question.answer_set.get(pk=request.POST['answer'])
     except (KeyError, Answer.DoesNotExist):
         return render(request, 'answer.html', {'question': question, 'error_message': request.POST['answer']})
     else:
         if answer.correct:
+			#return index page with 10 latest questions
             return render(request, "index.html", {"latest_questions": Question.objects.order_by('-added_at')[:10], "message": "Right!"})
         else:
+			#return list of answers whith choise bar
             return render(request, 'answer.html', {'question': question, 'error_message': 'Wrong!'})
